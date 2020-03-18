db524a51fbdad12fcfa78fb353f9be72148e9a40
Joepieler <be.campagne@live.nl>
2020-03-18 15:05:07 +0100

    unit test setup

---
+++ CMakeFiles/UnitTest.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UnitTest.dir/main.cpp.obj"
+  "UnitTest.exe"
+  "UnitTest.exe.manifest"
+  "UnitTest.pdb"
+  "libUnitTest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UnitTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <ostream>
+
+#define CATCH_CONFIG_MAIN
+#include "../externals/catch2/catch.hpp"
+
+TEST_CASE("test 1") {
+	int a = 1;
+	int b = 2;
+	REQUIRE(a == 1 );
+	REQUIRE(b == 2);
+}
